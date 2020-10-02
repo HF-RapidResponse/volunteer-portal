3629867b13559d4c7a33351dab8dc7c1c639c408
Earl Jay Caoile <unfie555@gmail.com>
2020-10-01 20:41:25 -0700

    added main scaffold files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
