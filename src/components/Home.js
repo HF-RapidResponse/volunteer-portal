cce34f5bb9f127fffca6371660b441ac805ebf28
Earl Jay Caoile <unfie555@gmail.com>
2020-10-03 19:11:40 -0700

    added volunteer listings airtable and wrote out dummy html for landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,35 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import '../styles/home.scss';
 
 function Home(props) {
   return (
     <>
-      <h2>Here is the home page!</h2>
-      <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
+      <h2>Welcome to the HF Volunteer Portal!</h2>
       <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
+        This space is for some motivational text that also explains what this
+        dashboard can be used for. Volunteering is fun!
       </p>
-      <Button variant="info" className="wide-btn">
-        I am a wide button!
-      </Button>
-      <h3>
-        Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in
-        45 BC
-      </h3>
-      <p>
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?"
-      </p>
+      <section>
+        <h3>Attend an Event</h3>
+        <p>
+          Insert a short, one to two sentence blurb about how easy it is to
+          attend Humanity Forward events.
+        </p>
+        <Button variant="info" className="wide-btn">
+          View HF Event Calendar
+        </Button>
+      </section>
+      <section>
+        <h3>Browse HF Volunteer Roles</h3>
+        <p>
+          Insert a short, one to two sentence blurb saying that we have
+          available roles that need to be filled.
+        </p>
+        <Button variant="info" className="wide-btn">
+          View HF Volunteer Roles
+        </Button>
+      </section>
     </>
   );
 }
--- src/components/Roles.js
+++ src/components/Roles.js
@@ -3,36 +3,20 @@ import React from 'react';
 function Roles(props) {
   return (
     <>
-      <h2>Here is the roles page!</h2>
-      <h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
+      <h2>Browse HF Volunteer Roles</h2>
       <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
+        Some messages about how there are available positions and roles and jobs
+        to fill in. Something else about this, too.
       </p>
-      <h3>
-        Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in
-        45 BC
-      </h3>
-      <p>
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?"
-      </p>
+      <iframe
+        className="airtable-embed"
+        src="https://airtable.com/embed/shrZmR6ahgCsyUBGy?backgroundColor=greenLight&viewControls=on"
+        frameBorder="0"
+        width="100%"
+        height="533"
+        title="volunteer-listings"
+        styles="background: transparent; border: 1px solid #ccc;"
+      ></iframe>
     </>
   );
 }
