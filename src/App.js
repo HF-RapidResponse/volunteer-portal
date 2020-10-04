535a1f6c44d8cecbebf866804524273724c541c7
Earl Jay Caoile <unfie555@gmail.com>
2020-10-03 19:37:06 -0700

    cleaned up GA code a bit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import { BrowserRouter } from 'react-router-dom';
 
 import Header from './components/Header';
 import PageViewSwitch from './components/PageViewSwitch';
-// import Signin from './components/Signin';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/base.scss';
@@ -13,9 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Container id="main-content">
-        <PageViewSwitch />
-      </Container>
+      <PageViewSwitch />
     </BrowserRouter>
   );
 }
--- src/components/PageViewSwitch.js
+++ src/components/PageViewSwitch.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
+import { Container } from 'react-bootstrap';
+import { Route, Switch, useLocation } from 'react-router-dom';
+
 import Home from './Home';
 import Calendar from './Calendar';
 import Roles from './Roles';
 import About from './About';
-import { Route, Switch, useLocation } from 'react-router-dom';
 
 // initialize GA
 // REACT_APP_GA_TRACKING_ID can be found in .env
@@ -19,14 +21,16 @@ function usePageViews() {
 function PageViewSwitch() {
   usePageViews();
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/calendar" exact component={Calendar} />
-      <Route path="/roles" exact component={Roles} />
-      <Route path="/about" exact component={About} />
-      {/* <Route path="/signin" exact component={Signin} /> */}
-      <Route path="*" component={() => '404 NOT FOUND'} />
-    </Switch>
+    <Container id="main-content">
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/calendar" exact component={Calendar} />
+        <Route path="/roles" exact component={Roles} />
+        <Route path="/about" exact component={About} />
+        {/* <Route path="/signin" exact component={Signin} /> */}
+        <Route path="*" component={() => '404 NOT FOUND'} />
+      </Switch>
+    </Container>
   );
 }
 
