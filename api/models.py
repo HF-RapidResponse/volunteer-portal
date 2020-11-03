c344541b8168785e8943b01411f391ac1491e1c3
Tripp Wickersham <trippersham@users.noreply.github.com>
2020-11-03 11:03:21 -0800

    Initiatives API (#34)
    * WIP
    
    * Initial functional API for VolunteerRoles, Volunteer Events,
    and Initiatives using live HF data:
    * API updated to include get_all and get_for_external_id routes for
    all three objects
    * DataSource and Dataset to handle pulling from external data sources
    * Models can be linked to Datasets with simple field mappings
    * Nested models are created automatically using introspection
    * Moar tests
    
    * Update CI & app->api
    
    * starting fixing lint things
    
    * Made accessing first key of dictionary more readable
    
    * supress lint issues...
    
    Co-authored-by: Tucker Kirven <tuckerkirven@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Union, List
 from enum import Enum
+from uuid import UUID, uuid4
+from datetime import datetime
 
 Url = str
 MarkdownText = str
+
+def generate_placeholder_image() -> Url:
+    return 'https://actblue-indigo-uploads.s3.amazonaws.com/uploads/list-editor/brandings/65454/header/image_url/f7edc334-2217-43b9-b707-5b7eaed92c1b-logo_stacked.svg'
 
 class Person(BaseModel):
     name: str
@@ -13,17 +18,45 @@ class Priority(Enum):
     HIGH = 'high'
     MEDIUM = 'medium'
     LOW = 'low'
+    NONE = None
 
 class VolunteerRole(BaseModel):
-    position_id: str
-    hero_image_url: Url
-    priority: Priority
+    role_uuid: UUID = uuid4()
+    role_external_id: str
+    name: str
+    hero_image_url: Url = generate_placeholder_image()
     signup_url: Url
-    hrff_team_lead: Optional[Person]
-    num_openings: int
-    min_weekly_time_commitment: int
-    max_weekly_time_commitment: int
-    overview: MarkdownText
-    what_you_will_learn: MarkdownText
-    responsibilites: MarkdownText
-    qualifications: MarkdownText
+    details_url: Optional[Url]
+    priority: Priority = Priority.LOW
+    point_of_contact: Optional[Person]
+    num_openings: int = 1
+    min_time_commitment: Optional[int]
+    max_time_commitment: Optional[int]
+    overview: Optional[MarkdownText]
+    benefits: Optional[MarkdownText]
+    responsibilites: Optional[MarkdownText]
+    qualifications: Optional[MarkdownText]
+
+class VolunteerEvent(BaseModel):
+    event_uuid: UUID = uuid4()
+    event_external_id: str
+    name: str
+    hero_image_url: Url = generate_placeholder_image()
+    signup_url: Url
+    details_url: Optional[Url]
+    start_datetime: datetime
+    end_datetime: Optional[datetime]
+    description: Optional[MarkdownText]
+    point_of_contact: Optional[Person]
+
+class Initiative(BaseModel):
+    initiative_uuid: UUID = uuid4()
+    initiative_external_id: str
+    name: str
+    details_url: Optional[Url]
+    title: str
+    hero_image_url: Url = generate_placeholder_image()
+    content: MarkdownText
+    roles: Optional[List[VolunteerRole]]
+    events: Optional[List[VolunteerEvent]]
+    highlightedItems: Optional[List[Union[VolunteerRole,VolunteerEvent]]]
