61a87047a9523f796813726f4e5ac1adda4531f9
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-11-02 19:41:43 -0500

    Update Recruitment Buttons to match Figma

--- client/src/components/RecruitmentSocialShare.js
+++ client/src/components/RecruitmentSocialShare.js
@@ -1,62 +1,33 @@
 import React from 'react';
 import {
-  TwitterShareButton,
   FacebookShareButton,
-  RedditShareButton,
-  FacebookMessengerShareButton,
-  LineShareButton,
-  TelegramShareButton,
-  ViberShareButton,
-  WhatsappShareButton,
-  EmailShareButton,
-
-  TwitterIcon,
-  FacebookIcon,
-  RedditIcon,
-  FacebookMessengerIcon,
-  LineIcon,
-  TelegramIcon,
-  ViberIcon,
-  WhatsappIcon,
-  EmailIcon,
+  TwitterShareButton
 } from 'react-share';
 import '../styles/recruitmentSocialShare.scss';
 
 function RecruitmentSocialShare() {
   const shareUrl = process.env.REACT_APP_SOCIAL_SHARE_URL;
-  const fbAppId = process.env.REACT_APP_FB_APP_ID;
-  const size = 48;
   const title = 'Volunteer for Hunanity Forward';
-  const description = 'There are many opportunities to support Humanity Forward and spread the vision of Andrew Yang. Join us!';
   return (
-    <div className="recruitment-social-share-buttons">
-      <TwitterShareButton url={shareUrl} title={title} related={['HumanityForward', 'AndrewYang']}>
-        <TwitterIcon size={size} round />
-      </TwitterShareButton>
-      <FacebookShareButton url={shareUrl} quote={title}>
-        <FacebookIcon size={size} round />
+    <div className="text-center">
+      <FacebookShareButton 
+        url={shareUrl} 
+        quote={title} 
+        className="btn btn-outline-info mb-3" 
+        resetButtonStyle={false}
+        aria-label="Share on Facebook">
+        Share on Facebook
       </FacebookShareButton>
-      <RedditShareButton url={shareUrl} title={title}>
-        <RedditIcon size={size} round />
-      </RedditShareButton>
-      <FacebookMessengerShareButton url={shareUrl} appId={fbAppId}>
-        <FacebookMessengerIcon size={size} round />
-      </FacebookMessengerShareButton>
-      <LineShareButton url={shareUrl} title={title}>
-        <LineIcon size={size} round />
-      </LineShareButton>
-      <TelegramShareButton url={shareUrl} title={title}>
-        <TelegramIcon size={size} round />
-      </TelegramShareButton>
-      <ViberShareButton url={shareUrl} title={title}>
-        <ViberIcon size={size} round />
-      </ViberShareButton>
-      <WhatsappShareButton url={shareUrl} title={title}>
-        <WhatsappIcon size={size} round />
-      </WhatsappShareButton>
-      <EmailShareButton url={shareUrl} subject={title} body={description}>
-        <EmailIcon size={size} round />
-      </EmailShareButton>
+      <br />
+      <TwitterShareButton 
+        url={shareUrl} 
+        title={title} 
+        related={['HumanityForward', 'AndrewYang']} 
+        className="btn btn-outline-info" 
+        resetButtonStyle={false}
+        aria-label="Share on Twitter">
+        Share on Twitter
+      </TwitterShareButton>
     </div>
   );
 }
