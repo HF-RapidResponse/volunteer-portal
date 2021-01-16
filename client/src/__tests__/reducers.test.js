effd10705e54949240a830376857fd9f847389e9
ecaoile <unfie555@gmail.com>
2021-01-15 18:02:08 -0800

    updated existing tests

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,18 +1,25 @@
-import userReducer, { login, logout } from '../store/user-slice.js';
+import userReducer, {
+  completeLogin,
+  completeLogout,
+} from '../store/user-slice.js';
 
 describe('user reducer', () => {
   const testUser = {
-    userID: '123456',
-    name: 'Bob Saget',
-    email: 'test@test.com',
+    ID: '12345',
+    email: 'andy@test.com',
+    username: 'andyfromtheblock',
+    name: 'Andy Yang',
+    city: 'New York City',
+    state: 'NY',
+    roles: ['President of Andy Club and the Superintendent', 'Mr. Grinch'],
     picture: 'https://via.placeholder.com/300/09f/fff.png',
   };
 
   it('can login and logout', () => {
-    let newState = userReducer({}, login(testUser));
+    let newState = userReducer({}, completeLogin(testUser));
 
     expect(newState.user).toMatchObject(testUser);
-    newState = userReducer(newState, logout());
+    newState = userReducer(newState, completeLogout());
     expect(newState.user).toBe(null);
   });
 });
