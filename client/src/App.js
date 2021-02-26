0a8df46817a256de86f40cb314af7aaef6d49cc7
ecaoile <unfie555@gmail.com>
2021-02-25 18:44:59 -0800

    added missing notification ref

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { withCookies } from 'react-cookie';
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-import Header from "./components/Header";
-import PageViewSwitch from "./components/PageViewSwitch";
-import Footer from "./components/Footer";
-import store from "./store";
+import Header from './components/Header';
+import PageViewSwitch from './components/PageViewSwitch';
+import Footer from './components/Footer';
+import store from './store';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/base.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/base.scss';
 
 const queryClient = new QueryClient();
 
