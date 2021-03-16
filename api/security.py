daaa220e12456d95a5c522162d7102610912337a
ecaoile <unfie555@gmail.com>
2021-03-15 22:05:08 -0700

    changed security .encrypt() to .hash()

--- api/security.py
+++ api/security.py
@@ -8,7 +8,7 @@ pwd_context = CryptContext(
 
 
 def encrypt_password(password):
-    return pwd_context.encrypt(password)
+    return pwd_context.hash(password)
 
 
 def check_encrypted_password(password, hashed):
