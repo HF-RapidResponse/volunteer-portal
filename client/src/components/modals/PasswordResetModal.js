6bee1b8d958b13e727d1c424f9695d0309c45d22
ecaoile <unfie555@gmail.com>
2021-03-26 21:15:57 -0700

    added success modal

--- client/src/components/modals/PasswordResetModal.js
+++ client/src/components/modals/PasswordResetModal.js
@@ -14,51 +14,70 @@ function PasswordResetModal(props) {
     handleSubmit,
     handleChange,
     data,
+    resetForm,
   } = useForm(attemptSendResetEmail);
 
+  const clearFormAndHide = () => {
+    props.onHide();
+    resetForm();
+  };
+
   return (
-    <Modal {...props} size="lg" centered>
+    <Modal {...props} size="lg" onExited={clearFormAndHide} centered>
       <Modal.Header closeButton />
-      <Modal.Body>
-        <h2 className="text-center mt-4 mb-4">Reset Password</h2>
-        <Form
-          noValidate
-          validated={validated}
-          onSubmit={handleSubmit}
-          className="mt-5 mb-5"
-        >
-          <Form.Group controlId="reset-password-form">
-            <Form.Label>Username or e-mail address</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter username or e-mail address (required)"
-              onChange={(e) => {
-                handleChange('username_or_email', e.target.value);
-              }}
-              isInvalid={
-                validated &&
-                (!data.username_or_email || Object.keys(errors).length)
-              }
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              This is not a valid username or e-mail format. Please try again.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <div className="text-center">
-            <Button variant="info" type="submit" className="mt-5 mb-3" block>
-              Reset Password
+      {submitted ? (
+        <Modal.Body>
+          <h4 className="m-4">
+            If we have you on record, we will send you an e-mail to reset your
+            password.
+          </h4>
+          <div className="text-right">
+            <Button
+              variant="outline-info"
+              className="m-4"
+              onClick={clearFormAndHide}
+            >
+              Return
             </Button>
           </div>
-        </Form>
-      </Modal.Body>
+        </Modal.Body>
+      ) : (
+        <Modal.Body>
+          <h2 className="text-center mt-4 mb-4">Reset Password</h2>
+          <Form
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+            className="mt-5 mb-5"
+          >
+            <Form.Group controlId="reset-password-form">
+              <Form.Label>Username or e-mail address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username or e-mail address (required)"
+                onChange={(e) => {
+                  handleChange('username_or_email', e.target.value);
+                }}
+                isInvalid={
+                  validated &&
+                  (!data.username_or_email || !!errors.usernameOrEmail)
+                }
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                This is not a valid username or e-mail format. Please try again.
+              </Form.Control.Feedback>
+            </Form.Group>
+            <div className="text-center">
+              <Button variant="info" type="submit" className="mt-5 mb-3" block>
+                Reset Password
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      )}
     </Modal>
   );
 }
 
-// const mapDispatchToProps = {
-//   attemptSendResetEmail,
-// };
-
-// export default connect(null, mapDispatchToProps)(PasswordReset);
 export default PasswordResetModal;
