4dc7962ea22063daf125ead602e5ae35ae1b16c8
ecaoile <unfie555@gmail.com>
2021-04-02 12:14:49 -0700

    pushing justin suggestions

--- client/src/__tests__/Header.test.js
+++ client/src/__tests__/Header.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, fireEvent, screen } from '@testing-library/react';
 
-import Header from '../components/Header';
+import Header from 'components/Header';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store/index';
@@ -24,8 +24,8 @@ describe('header component', () => {
     const eventsTag = screen.getByText('Event Calendar');
     expect(eventsTag).toBeDefined();
 
-    const vounteerTag = screen.getByText('Volunteer Roles');
-    expect(vounteerTag).toBeDefined();
+    const volunteerTag = screen.getByText('Volunteer Roles');
+    expect(volunteerTag).toBeDefined();
 
     const returnTag = screen.getByText('Return to Parent Site');
     expect(returnTag).toBeDefined();
