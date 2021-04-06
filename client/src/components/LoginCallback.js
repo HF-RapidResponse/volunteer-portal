ff4645f1c6704a2a1c79848a3b17cc2b65df935b
ecaoile <unfie555@gmail.com>
2021-04-05 23:22:19 -0700

    undoing setTimeout change; why did that even happen?!

--- client/src/components/LoginCallback.js
+++ client/src/components/LoginCallback.js
@@ -13,7 +13,7 @@ function LoginCallback(props) {
       if (window.location.pathname.includes('/login_callback')) {
         setErrorLoading(true);
       }
-    }, 5000);
+    }, 3000);
   }, []);
 
   useEffect(() => {
