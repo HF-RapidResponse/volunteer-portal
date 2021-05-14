8429585085eb4eba6bcef7e5932c2f43a3ef10b6
ecaoile <unfie555@gmail.com>
2021-05-13 23:34:42 -0700

    think I added logic to allow for null hero_image-url

--- api/models/volunteer_event.py
+++ api/models/volunteer_event.py
@@ -7,12 +7,14 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql import func
 from uuid import uuid4
 
+
 class VolunteerEvent(AirtableRow, Base):
     __tablename__ = 'events'
 
-    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True, nullable=False)
+    uuid = Column(UUID(as_uuid=True), primary_key=True,
+                  default=uuid4, unique=True, nullable=False)
     event_name = Column('event_name', String(255), nullable=False)
-    hero_image_urls = Column('event_graphics', ARRAY(JSON))
+    hero_image_urls = Column('event_graphics', ARRAY(JSON), nullable=True)
     signup_url = Column('signup_link', Text, nullable=False)
     start_datetime = Column('start', DateTime, nullable=False)
     end_datetime = Column('end', DateTime)
@@ -20,8 +22,8 @@ class VolunteerEvent(AirtableRow, Base):
 
     @hybrid_property
     def hero_image_url(self):
-        return self.hero_image_urls[0]['url'] if self.hero_image_urls else placeholder_image()
+        return self.hero_image_urls[0]['url'] if self.hero_image_urls and len(self.hero_image_urls) > 0 else placeholder_image()
 
     def __repr__(self):
         return "VolunteerEvent(uuid='%s', external_id='%s', name='%s')" % (
-                                self.uuid, self.external_id, self.event_name)
+            self.uuid, self.external_id, self.event_name)
