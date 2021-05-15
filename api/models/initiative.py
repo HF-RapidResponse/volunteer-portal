b413b2c9fd070f799f91b88c0dcc2319efc812c1
ecaoile <unfie555@gmail.com>
2021-05-14 23:46:19 -0700

    added logic to initiatives as well, removed unused imports in auth.py

--- api/models/initiative.py
+++ api/models/initiative.py
@@ -11,25 +11,27 @@ from pydantic import validator
 from sqlalchemy.sql import func
 from uuid import uuid4
 
+
 class Initiative(AirtableRow, Base):
     __tablename__ = 'initiatives'
 
-    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True, nullable=False)
+    uuid = Column(UUID(as_uuid=True), primary_key=True,
+                  default=uuid4, unique=True, nullable=False)
     initiative_name = Column('initiative_name', String(255), nullable=False)
     order = Column('order', Integer, nullable=False)
     details_url = Column('details_link', String(255), nullable=True)
-    hero_image_urls = Column('hero_image_urls', ARRAY(JSON))
+    hero_image_urls = Column('hero_image_urls', ARRAY(JSON), nullable=True)
     content = Column('description', Text, nullable=False)
     role_ids = Column('roles', ARRAY(String), default=[], nullable=False)
     event_ids = Column('events', ARRAY(String), default=[], nullable=False)
 
     def __repr__(self):
         return "Initiative(initiative_uuid='%s', initiative_external_id='%s', name='%s')" % (
-                                self.uuid, self.external_id, self.name)
+            self.uuid, self.external_id, self.name)
 
     @hybrid_property
     def hero_image_url(self):
-        return self.hero_image_urls[0]['url'] if self.hero_image_urls else placeholder_image()
+        return self.hero_image_urls[0]['url'] if self.hero_image_urls and len(self.hero_image_urls) > 0 else placeholder_image()
 
     @hybrid_property
     def roles_count(self):
@@ -38,6 +40,7 @@ class Initiative(AirtableRow, Base):
     @hybrid_property
     def events_count(self):
         return len(self.event_ids)
+
 
 class NestedInitiative(Initiative, Base):
     @hybrid_property
@@ -49,32 +52,35 @@ class NestedInitiative(Initiative, Base):
         return self.events_rel.filter(VolunteerEvent.external_id.in_(self.event_ids))\
                               .order_by(VolunteerEvent.start_datetime.asc()).all()
 
+
 # Initiative & Roles
-initiaitves_roles_selection = select([func.unnest(Initiative.role_ids).label("role_external_id"), Initiative.external_id.label("initiative_external_id")]).alias()
+initiaitves_roles_selection = select([func.unnest(Initiative.role_ids).label(
+    "role_external_id"), Initiative.external_id.label("initiative_external_id")]).alias()
 # Todo: Figure out how to set roles on an initiative and have them save in the same transaction
 Initiative.roles_rel = relationship(VolunteerRole, secondary=initiaitves_roles_selection,
-                          primaryjoin=VolunteerRole.external_id == initiaitves_roles_selection.c.role_external_id,
-                          secondaryjoin=initiaitves_roles_selection.c.initiative_external_id == Initiative.external_id,
-                          lazy='dynamic',
-                          viewonly=True)
+                                    primaryjoin=VolunteerRole.external_id == initiaitves_roles_selection.c.role_external_id,
+                                    secondaryjoin=initiaitves_roles_selection.c.initiative_external_id == Initiative.external_id,
+                                    lazy='dynamic',
+                                    viewonly=True)
 
 VolunteerRole.initiatives_rel = relationship(Initiative, secondary=initiaitves_roles_selection,
-                          primaryjoin=VolunteerRole.external_id == initiaitves_roles_selection.c.role_external_id,
-                          secondaryjoin=initiaitves_roles_selection.c.initiative_external_id == Initiative.external_id,
-                          # lazy='dynamic',
-                          viewonly=True)
+                                             primaryjoin=VolunteerRole.external_id == initiaitves_roles_selection.c.role_external_id,
+                                             secondaryjoin=initiaitves_roles_selection.c.initiative_external_id == Initiative.external_id,
+                                             # lazy='dynamic',
+                                             viewonly=True)
 
 # Initiative & Events
-initiaitves_events_selection = select([func.unnest(Initiative.event_ids).label("event_external_id"), Initiative.external_id.label("initiative_external_id")]).alias()
+initiaitves_events_selection = select([func.unnest(Initiative.event_ids).label(
+    "event_external_id"), Initiative.external_id.label("initiative_external_id")]).alias()
 # Todo: Figure out how to set events on an initiative and have them save in the same transaction
 Initiative.events_rel = relationship(VolunteerEvent, secondary=initiaitves_events_selection,
-                          primaryjoin=VolunteerEvent.external_id == initiaitves_events_selection.c.event_external_id,
-                          secondaryjoin=initiaitves_events_selection.c.initiative_external_id == Initiative.external_id,
-                          lazy='dynamic',
-                          viewonly=True)
+                                     primaryjoin=VolunteerEvent.external_id == initiaitves_events_selection.c.event_external_id,
+                                     secondaryjoin=initiaitves_events_selection.c.initiative_external_id == Initiative.external_id,
+                                     lazy='dynamic',
+                                     viewonly=True)
 
 VolunteerEvent.initiatives_rel = relationship(Initiative, secondary=initiaitves_events_selection,
-                          primaryjoin=VolunteerEvent.external_id == initiaitves_events_selection.c.event_external_id,
-                          secondaryjoin=initiaitves_events_selection.c.initiative_external_id == Initiative.external_id,
-                          # lazy='dynamic',
-                          viewonly=True)
+                                              primaryjoin=VolunteerEvent.external_id == initiaitves_events_selection.c.event_external_id,
+                                              secondaryjoin=initiaitves_events_selection.c.initiative_external_id == Initiative.external_id,
+                                              # lazy='dynamic',
+                                              viewonly=True)
